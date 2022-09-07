c97fc9dd6cd9ca4008e7fcd172d3ec565d51f663
Herman Dmukhovskiy <dmukhovskiy_g@mail.ru>
2022-09-08 01:13:12 +0300

    ADDED: Swagger(documentation)

--- src/main/java/by/tms/restproject/controller/UserController.java
+++ src/main/java/by/tms/restproject/controller/UserController.java
@@ -2,9 +2,16 @@ package by.tms.restproject.controller;
 
 import by.tms.restproject.dao.InMemoryUserDao;
 import by.tms.restproject.entity.User;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.UsesSunHttpServer;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,17 +23,21 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User")
+//@OpenAPIDefinition()
 public class UserController {
     @Autowired
     private InMemoryUserDao inMemoryUserDao;
 
     @PostMapping
-    public ResponseEntity<?> save(@Valid @RequestBody User user) {
+    @Operation(summary = "Save User")
+    public ResponseEntity<User> save(@Valid @RequestBody User user) {
         User save = inMemoryUserDao.save(user);
         return new ResponseEntity<>(save, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Find All Users")
     public ResponseEntity<List<User>> findAll() {
         List<User> all = inMemoryUserDao.findAll();
         if (all.isEmpty()) {
@@ -36,6 +47,7 @@ public class UserController {
     }
 
     @GetMapping("/id")
+    @Operation(summary = "Find User By ID")
     public ResponseEntity<User> findById(long id) {
         Optional<User> byId = inMemoryUserDao.findById(id);
         if (byId.isPresent()) {
@@ -45,6 +57,7 @@ public class UserController {
     }
 
     @GetMapping("/username")
+    @Operation(summary = "Find User Bu Username")
     public ResponseEntity<User> findByUsername(String username) {
         Optional<User> byUsername = inMemoryUserDao.getByUsername(username);
         if (byUsername.isPresent()) {
@@ -54,7 +67,8 @@ public class UserController {
     }
 
     @PutMapping
-    public ResponseEntity<User> update(@RequestBody User user) {
+    @Operation(summary = "Update User")
+    public ResponseEntity<User> update(@Valid @RequestBody User user) {
         Optional<User> update = inMemoryUserDao.update(user);
         if (update.isPresent()) {
             return ResponseEntity.ok(update.get());
@@ -63,7 +77,8 @@ public class UserController {
     }
 
     @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody User user) {
+    @Operation(summary = "Delete User")
+    public ResponseEntity<?> delete(@Valid @RequestBody User user) {
         if (inMemoryUserDao.delete(user)) {
             return ResponseEntity.ok().build();
         }
@@ -72,6 +87,7 @@ public class UserController {
 
 
     @DeleteMapping("/id")
+    @Operation(summary="Delete User By ID")
     public ResponseEntity<User> deleteById(long id) {
         Optional<User> user = inMemoryUserDao.deleteById(id);
         if (user.isPresent()) {
